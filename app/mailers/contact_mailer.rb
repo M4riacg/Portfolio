5460cc6cb8b8cec59c6ffa487faab07a433b4ab3
María <m4riacg@gmail.com>
2016-04-24 16:00:20 +0200

    Mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ApplicationMailer
   default :to => "m4riacg@gmail.com"
-  layout 'mailer'
 
   def new_message(message)
     @message = message
-    mail(to: "m4riacg@gmail.com", subject: 'Welcome to My Awesome Site')
+    #mail(to: "m4riacg@gmail.com", subject: 'Welcome to My Awesome Site')
+    mail(from: @message.email, subject: @message.name, body: @message.body)
   end
 end
